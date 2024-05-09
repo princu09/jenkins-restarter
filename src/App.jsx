0478376e31070d43d7774f08ab4902ff3ac5861d
Prince K. Patel <56663010+princu09@users.noreply.github.com>
2024-05-10 04:22:06 +0530

    jenkins server restarter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Jenkins from "./Jenkins";
-import Websites from "./Websites";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Navigate to="/websites" />} />
         <Route path="/jenkins" element={<Jenkins />} />
         <Route path="/websites" element={<Websites />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 };
